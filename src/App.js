12b9666b239b78ae1f9b37f7975ce144d44754e1
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-11-04 00:02:18 -0400

    signIn Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
 import "./App.css";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
 import Navigation from "./components/Navigation/Navigation";
 import Rank from "./components/Rank/Rank";
-import Particles from "./components/Particles/Particles";
 import FaceRrecognition from "./components/FaceRrecognition/FaceRrecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 // import Clarifai from "clarifai";
 
 // const app = new Clarifai.App({
@@ -19,6 +21,8 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "signin",
+      isSignedIn: false,
     };
   }
 
@@ -41,15 +45,37 @@ class App extends Component {
     this.setState({ box });
   };
 
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+
   onInputChange = (e) => {
     console.log(e.target.value);
     this.setState({ input: e.target.value });
   };
 
+  
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+
   onButtonSubmit = async () => {
     try {
       console.log(this.state.input);
-      await this.setState({ imageUrl: this.state.input });
+      // await this.setState({ imageUrl: this.state.input });
       // app.models
       //   .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       //   .then((res) => {
@@ -65,26 +91,28 @@ class App extends Component {
   };
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Particles />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRrecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <ParticlesBg type="fountain" num={20} bg={true} />
 
-        {/* {      
+        <Navigation isSignedIn={isSignedIn}  onRouteChange={this.onRouteChange} />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRrecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+            <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        )}
 
-
-   
-
-
-
-    <FaceRrecognition />} */}
       </div>
     );
   }
