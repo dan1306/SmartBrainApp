29b9aae049de003d7dea993d9363354fb5beeb72
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-11-06 19:51:54 -0500

    deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const favicon = require("serve-favicon");
 const db = knex({
     client: "pg",
     connection: {
-      host: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: true
     },
   });
